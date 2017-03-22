e692bc54c336ffa12c9c3c8f8b98fb49cc91b597
imbamboo <tonydev@foxmail.com>
2017-03-22 17:09:58 +0800

    规则构想

---
+++ xvalidation.js
@@ -0,0 +1,133 @@
+"use strict";
+
+/**
+ * rules:
+ * {
+ *      name:"required",
+ * 
+ *      age:{
+ *          required:true,
+ *          number:true,
+ *          // 包括max值，如果不想包括max值，即希望大于的操作，改用 max_out
+ *          max:{ 
+ *              value:60, 
+ *              // 自定义提示消息内容
+ *              msg:"年龄不得超过60岁" 
+ *          },
+ *          // 跟max类似, min_out
+ *          min:18
+ *      },
+ * 
+ *      password:{
+ *          required:true,
+ *          // 默认会去除字符串首尾空格，如果不希望这样，可以定义 trim:false
+ *          trim:false,
+ *      }
+ *      content:{
+ *          maxlength:8000,
+ *          minlength:100,
+ *          // 如果在规则中定义了msg，那么该条规则下其它细则没有自定义msg的，默认以此msg显示。
+ *          // 细则有定义msg的，当然按细则 msg 显示。
+ *          msg:"至少100个字符，最多8000个字符。"
+ *      }
+ * 
+ *      email:{
+ *          email:true
+ *          // other pre-set format:
+ *          // number
+ *          // letter 英文字母[a-zA-Z]
+ *          // Chinese.mobileNo
+ *          // Chinese.IDNo
+ *          // Chinese.phoneNo
+ *      }
+ *  
+ *      phoneNo:/\d{13}/
+ * 
+ *      faxNo:{
+ *          required:true,
+ *          format:/\d{3,4}-\d{7,8}/
+ *      }
+ * 
+ *      // person[sex] is a group of radion button
+ *      "person[sex]":{
+ *          required:true,
+ *      },
+ * 
+ *      // hobby is a group of checkbox
+ *      hobby:{
+ *          custom:function(){
+ *              // 随便假设一下，年龄50岁以上的，不能选择 football
+ *              if(this.age > 50 && this.hobby.indexOf("football") !== -1){
+ *                  return false;
+ *              }
+ *              return true;
+ *          }
+ *      }
+ * }
+ * 
+ */
+(function ($) {
+    var rules = {
+        "required": {
+            msg: "the field is required.",
+            test: function () {
+                if (this === null || this === undefined || this.length === 0) {
+                    return false;
+                }
+
+                return true;
+            }
+        },
+
+        "number": {
+            msg: "the field expects a number.",
+            test: function () {
+                var expr = /^\-?\d+(\.\d+)?$/;
+                return expr.test(this);
+            }
+        },
+
+        "max": {
+            msg: "the inputed number should less or equals to $value.",
+            test: function ($value) {
+                return parseFloat(this) <= $value;
+            }
+        },
+
+        "max_out": {
+            msg: "the inputed number should less to $value.",
+            test: function ($value) {
+                return parseFloat(this) < $value;
+            }
+        },
+
+        "min": {
+            msg: "the inputed number should greater or equals to $value.",
+            test: function ($value) {
+                return parseFloat(this) >= $value;
+            }
+        },
+
+        "min_out": {
+            msg: "the inputed number should greater to $value.",
+            test: function ($value) {
+                return parseFloat(this) > $value;
+            }
+        },
+    };
+
+    var methods = {
+        init: function (options) {
+
+        },
+        valid: function () {
+
+        }
+    };
+
+    $.fn.validator = function (method) {
+
+
+        return this;
+    };
+})(jQuery);
